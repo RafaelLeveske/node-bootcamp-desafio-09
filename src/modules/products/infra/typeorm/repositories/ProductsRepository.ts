552e04e329343d439a600e01ba312c8c85602e84
Rafael Leveske <rafaelleveske@gmail.com>
2021-01-02 17:26:10 -0300

    Falta services do modulo orders

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,8 +1,9 @@
-import { getRepository, Repository, In } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
 import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import AppError from '@shared/errors/AppError';
 import Product from '../entities/Product';
 
 interface IFindProducts {
@@ -21,21 +22,57 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProduct = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return findProduct;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    return this.ormRepository.findByIds(products);
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsList = await this.ormRepository.findByIds(products);
+
+    const updatedProductsList = productsList.map(productFromList => {
+      const productToSubtract = products.find(
+        product => product.id === productFromList.id,
+      );
+
+      if (!productToSubtract) {
+        throw new AppError(
+          'Error when updating product quantity, product id not found',
+        );
+      }
+
+      Object.assign(productFromList, {
+        quantity: productFromList.quantity - productToSubtract.quantity,
+      });
+
+      return productFromList;
+    });
+
+    await this.ormRepository.save(updatedProductsList);
+
+    return updatedProductsList;
   }
 }
 
