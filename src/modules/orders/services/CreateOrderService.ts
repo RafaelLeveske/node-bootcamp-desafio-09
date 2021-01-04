b4232ad5ba6163f62b6e7ca05b66e4783ae37900
Rafael Leveske <rafaelleveske@gmail.com>
2021-01-04 12:16:36 -0300

    Desafio finalizado

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,55 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer does not exists');
+    }
+
+    const productList = await this.productsRepository.findAllById(products);
+
+    const formattedProductList = products.map(product => {
+      const productEntity = productList.find(
+        productFromList => productFromList.id === product.id,
+      );
+
+      if (!productEntity) {
+        throw new AppError('Invalid product');
+      }
+
+      if (product.quantity > productEntity.quantity) {
+        throw new AppError(
+          `The quantity of ${productEntity.name} in stock is insufficient`,
+        );
+      }
+
+      return {
+        product_id: product.id,
+        price: productEntity.price,
+        quantity: product.quantity,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: formattedProductList,
+    });
+
+    return order;
   }
 }
 
